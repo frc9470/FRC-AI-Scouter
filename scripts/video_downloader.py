294eebe9bf5876a8585835514913a69f18ad40df
CodeMyGame1 <40456953+CodeMyGame1@users.noreply.github.com>
2026-03-21 13:54:05 -0700

    chore: make video downloader accept URL as shell argument

--- scripts/video_downloader.py
+++ scripts/video_downloader.py
@@ -1,5 +1,17 @@
+import sys
+
 from yt_dlp import YoutubeDL
 
-URLS = ["https://www.youtube.com/watch?v=41kgSdbq3bI"]
-with YoutubeDL() as ydl:
-    ydl.download(URLS)
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python scripts/video_downloader.py /path/to/video")
+        sys.exit(1)
+
+    print("got here")
+
+    URLS = [sys.argv[1]]
+    with YoutubeDL() as ydl:
+        ydl.download(URLS)
+
+if __name__ == "__main__":
+    main()
