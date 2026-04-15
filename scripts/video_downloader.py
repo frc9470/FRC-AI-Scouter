a336096028c70592175595378802e375c28c97f3
CodeMyGame1 <40456953+CodeMyGame1@users.noreply.github.com>
2026-04-14 18:48:47 -0700

    chore: allow video downloader to load browser cookies for yt-dlp

--- scripts/video_downloader.py
+++ scripts/video_downloader.py
@@ -3,14 +3,20 @@ import sys
 from yt_dlp import YoutubeDL
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python scripts/video_downloader.py /path/to/video")
+    if len(sys.argv) < 3:
+        print(
+            "Usage: python scripts/video_downloader.py /path/to/video " +
+            "<browser profile>"
+        )
         sys.exit(1)
 
     print("got here")
 
     URLS = [sys.argv[1]]
-    with YoutubeDL() as ydl:
+    ydl_opts = {
+        "cookiesfrombrowser": ("chrome", sys.argv[2])
+    }
+    with YoutubeDL(ydl_opts) as ydl:
         ydl.download(URLS)
 
 if __name__ == "__main__":
